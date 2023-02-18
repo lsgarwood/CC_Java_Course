a393274b3dbcc81a64d83313a759d11dfc07807a
Lauren Garwood <ls_garwood@yahoo.co.uk>
2023-02-18 22:55:02 +0000

    Classes

--- src/lessons/ObjectOrientedJava.java
+++ src/lessons/ObjectOrientedJava.java
@@ -74,8 +74,80 @@ public class ObjectOrientedJava {
 	    		System.out.println(myCar.color); // Prints: red
 	  		}
 		}
-			*/
+		*/
 		
+		/* Now that our constructor has a parameter, we must pass values into the method call. These values are referred to as arguments; once they are passed in, they will be used to give the instance fields initial value.
+
+		Here we create an instance, ferrari, in the main() method with "red" as its color field:
+		
+		public class Car {
+		  String color;
+		 
+		  public Car(String carColor) {
+		    // assign parameter value to instance field
+		    color = carColor;
+		  }
+		 
+		  public static void main(String[] args) {
+		    // parameter value supplied when calling constructor
+		    Car ferrari = new Car("red");
+		  }
+		}
+		We pass the String value "red" to our constructor method call: new Car("red");.
+		
+		The type of the value given to the invocation must match the type declared by the parameter.
+		
+		Inside the constructor, the parameter carColor refers to the value passed in during the invocation: "red". This value is assigned to the instance field color.
+		
+		color has already been declared, so we don’t specify the type during assignment.
+		
+		The object, ferrari, holds the state of color as an instance field referencing the value "red".
+		
+		We access the value of this field with the dot operator (.):
+		
+		
+		accessing a field:
+		objectName.fieldName
+		
+		 
+		ferrari.color;
+		// "red" */
+		
+		//Multiple Fields
+		/* public class Car {
+			  String color;
+			  // new fields!
+			  boolean isRunning;
+			  int velocity;
+			 
+			  // new parameters that correspond to the new fields
+			  public Car(String carColor, boolean carRunning, int milesPerHour) {
+			    color = carColor;
+			    // assign new parameters to the new fields
+			    isRunning = carRunning;
+			    velocity = milesPerHour;
+			  }
+			 
+			  public static void main(String[] args) {
+			    // new values passed into the method call
+			    Car ferrari = new Car("red", true, 27);
+			    Car renault = new Car("blue", false, 70);
+			 
+			    System.out.println(renault.isRunning);
+			    // false
+			    System.out.println(ferrari.velocity);
+			    // 27
+			  }
+			}
+			//The constructor now has multiple parameters to receive values for the new fields. We still specify the type as well as the name for each parameter.
+
+			//Ordering matters! We must pass values into the constructor invocation in the same order that they’re listed in the parameters.
+
+			// values match types, no error
+			Car honda = new Car("green", false, 0);
+			 
+			// values do not match types, error!
+			Car junker = new Car(true, 42, "brown");*/
 	}
 
 }
